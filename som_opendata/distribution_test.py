a47b66fd66322eb499f6a43f6540fccc6b14bbf5
Benjami Ramos Solis <benjami.ramos@somenergia.coop>
2018-06-28 14:16:07 +0200

    test__parse_tsv__2col_1row

--- som_opendata/distribution_test.py
+++ som_opendata/distribution_test.py
@@ -4,12 +4,14 @@ from distribution import parse_tsv
 
 class Distribution_Test(unittest.TestCase):
     
-
-    def test__parse_tsv(self):
+    def test__parse_tsv__1col_1row(self):
         fixture = 'item'
         self.assertEqual(parse_tsv(fixture), [['item']])
 
 
+    def test__parse_tsv__2col_1row(self):
+        fixture = 'item1\titem2'
+        self.assertEqual(parse_tsv(fixture), [['item1', 'item2']])
 
 
 # vim: et sw=4 ts=4
