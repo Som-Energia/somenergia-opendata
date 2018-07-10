2d9bb0498013b8532a17a40c07784c55173bf393
Benjami Ramos Solis <benjami.ramos@somenergia.coop>
2018-07-10 12:29:47 +0200

    Arreglada l'estructura de test_socis

--- som_opendata/socis/test_socis.py
+++ som_opendata/socis/test_socis.py
@@ -2,73 +2,78 @@ import unittest
 from flask import current_app
 from dbutils import csvTable
 import requests
+from ..app import app
+from yamlns import namespace as ns
+
 
 
 class TestSocis(unittest.TestCase):
 
-    # def setUp(self):
-    #     self.client = app.test_client()
+    def setUp(self):
+        self.client = app.test_client()
 
+    def get(self, *args, **kwds):
+        return self.client.get(*args,**kwds)
 
     def test_socis_ok(self):
-        #res = self.get('htttp://127.0.0.1:5001/socis')
-        res = requests.get('http://localhost:5001/socis')
+        res = self.get('htttp://127.0.0.1:5001/socis')
+        #res = requests.get('http://localhost:5001/socis')
 
         assert res.status_code == 200
 
+    from ..testutils import assertNsEqual
 
     def test_socis_response(self):
-        res = requests.get('http://localhost:5001/socis')
-
-        assert res.content.split()[0] == 'socis:'
+        res = self.get('http://localhost:5001/socis')
+        assert res.data == ns(socis=88588).dump()
 
 
     def test_socis_pais_ok(self):
-        res  = requests.get('http://localhost:5001/socis/ES')
+        res  = self.get('http://localhost:5001/socis/ES')
 
         assert res.status_code == 200
 
 
     def test_socis_pais_response(self):
-        res  = requests.get('http://localhost:5001/socis/ES')
+        res  = self.get('http://localhost:5001/socis/ES')
 
-        assert res.content.split()[0] == 'socis:'
+        assert res.data == ns(socis=87481).dump()
 
 
     def test_socis_pais_ccaa_ok(self):
-        res  = requests.get('http://localhost:5001/socis/ES/9')
+        res  = self.get('http://localhost:5001/socis/ES/9')
 
         assert res.status_code == 200
 
 
     def test_socis_pais_ccaa_response(self):
-        res  = requests.get('http://localhost:5001/socis/ES/9')
+        res  = self.get('http://localhost:5001/socis/ES/9')
 
-        assert res.content.split()[0] == 'socis:'
+        assert res.data == ns(socis=55454).dump()
 
 
     def test_socis_pais_ccaa_prov_ok(self):
-        res  = requests.get('http://localhost:5001/socis/ES/9/17')
+        res  = self.get('http://localhost:5001/socis/ES/9/17')
 
         assert res.status_code == 200
 
 
     def test_socis_pais_ccaa_prov_response(self):
-        res  = requests.get('http://localhost:5001/socis/ES/9/17')
+        res  = self.get('http://localhost:5001/socis/ES/9/17')
 
-        assert res.content.split()[0] == 'socis:'
+        assert res.data == ns(socis=6735).dump()
 
 
     def test_socis_pais_ccaa_prov_municipi_ok(self):
-        res  = requests.get('http://localhost:5001/socis/ES/9/17/17079')
+        res  = self.get('http://localhost:5001/socis/ES/9/17/17079')
 
         assert res.status_code == 200
 
 
     def test_socis_pais_ccaa_prov_municipi_response(self):
-        res  = requests.get('http://localhost:5001/socis/ES/9/17/17079')
+        res  = self.get('http://localhost:5001/socis/ES/9/17/17079')
 
-        assert res.content.split()[0] == 'socis:'
+        assert res.data == ns(socis=1821).dump()
 
 
 unittest.TestCase.__str__ = unittest.TestCase.id
