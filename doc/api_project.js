836ea6648e7000e93a8f9fb43972d42ced907271
Benjami Ramos Solis <benjami.ramos@somenergia.coop>
2018-08-21 16:49:09 +0200

    Documentation 1.0

--- doc/api_project.js
+++ doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-07-09T09:19:08.132Z",
+    "time": "2018-08-21T14:11:43.979Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
--- som_opendata/printer/printer.py
+++ som_opendata/printer/printer.py
@@ -31,7 +31,167 @@ def extractQueryParam(location_filter_req, queryName, objectName):
         location_filter_req[objectName] = queryParam
 
 
-@printer_module.route('/<field:field>')
+"""
+@api {get} /printer/:field[/by/:aggregateLevel]/on/:ondate|/frequency/:frequency[/from/:fromdate][/to/:todate]?queryFilter=:locationFilters
+
+@apiVersion 1.0.1
+@apiName OpenData
+@apiGroup Printer
+@apiDescription Retorna un yaml amb la distribució desitjada repartida en espai - temps
+@apiParam {String="contracts","members"} field  Firstname of the User.
+@apiParam {String} [ondate]  Date in iso format.
+@apiParam {String} [fromdate=2012-01-01]  Date in iso format. 
+@apiParam {String} [todate=2018-08-01]  Date in iso format. 
+@apiParam {String="countries","ccaas","states","cities"} [aggregateLevel=world]  Aggregate level response.
+@apiParam {String="yearly","monthly"} [frequency]  Frequency response.
+@apiParam {String="contry","ccaa","state","city"} [queryFilter] Query Geographical filter.
+
+
+@apiSampleRequest http://192.168.1.5:5001/printer/contracts/by/ccaas/yearly/from/2010-01-01/to/2013-01-01?country=ES
+@apiSuccessExample {yaml} Success-Response:
+    HTTP/1.1 200OK
+    {
+        dates:
+        - 2010-01-01
+        - 2011-01-01
+        - 2012-01-01
+        - 2013-01-01
+        data:
+        - 0
+        - 0
+        - 277
+        - 3197
+        countries:
+          ES:
+            name: España
+            data:
+            - 0
+            - 0
+            - 277
+            - 3197
+            ccaas:
+              '01':
+                name: Andalucia
+                data:
+                - 0
+                - 0
+                - 0
+                - 48
+              '02':
+                name: Aragón
+                data:
+                - 0
+                - 0
+                - 0
+                - 124
+              '03':
+                name: Asturias, Principado de
+                data:
+                - 0
+                - 0
+                - 0
+                - 13
+              '04':
+                name: Baleares, Islas
+                data:
+                - 0
+                - 0
+                - 1
+                - 235
+              '05':
+                name: Canarias
+                data:
+                - 0
+                - 0
+                - 0
+                - 0
+              '06':
+                name: Cantabria
+                data:
+                - 0
+                - 0
+                - 0
+                - 12
+              08:
+                name: Castilla - La Mancha
+                data:
+                - 0
+                - 0
+                - 0
+                - 28
+              '07':
+                name: Castilla y León
+                data:
+                - 0
+                - 0
+                - 0
+                - 24
+              09:
+                name: Cataluña
+                data:
+                - 0
+                - 0
+                - 256
+                - 2054
+              '10':
+                name: Comunidad Valenciana
+                data:
+                - 0
+                - 0
+                - 11
+                - 224
+              '11':
+                name: Extremadura
+                data:
+                - 0
+                - 0
+                - 0
+                - 14
+              '12':
+                name: Galicia
+                data:
+                - 0
+                - 0
+                - 0
+                - 24
+              '13':
+                name: Madrid, Comunidad de
+                data:
+                - 0
+                - 0
+                - 3
+                - 145
+              '14':
+                name: Murcia, Región de
+                data:
+                - 0
+                - 0
+                - 0
+                - 11
+              '15':
+                name: Navarra, Comunidad Foral de
+                data:
+                - 0
+                - 0
+                - 6
+                - 151
+              '16':
+                name: País Vasco
+                data:
+                - 0
+                - 0
+                - 0
+                - 53
+              '17':
+                name: Rioja, La
+                data:
+                - 0
+                - 0
+                - 0
+                - 37
+    }
+"""
+
 @printer_module.route('/<field:field>/on/<isodate:ondate>')
 @printer_module.route('/<field:field>/by/<aggregateLevel:al>')
 @printer_module.route('/<field:field>/by/<aggregateLevel:al>/on/<isodate:ondate>')
