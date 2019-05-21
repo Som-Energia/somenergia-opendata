72359e08f8a789da4968d81c0ade6523f2e7d854
David García Garzón <voki@canvoki.net>
2019-05-21 12:53:47 +0200

    wip: script to generate the data

---
+++ generate_data.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import sys
+from som_opendata.oldapi import (
+    membersSparse,
+    contractsSparse,
+)
+from som_opendata.common import (
+    dateSequenceMonths,
+    readQuery,
+    dateSequenceWeeksMonths,
+    utf8,
+    )
+
+from yamlns.dateutils import Date
+from dbutils import csvTable
+import io
+from consolemsg import step
+
+
+fromdate = Date('2011-01-01')
+todate = Date.today()
+
+dates=dateSequenceMonths(fromdate, todate)
+
+
+for metric, generator in dict(
+        members=membersSparse,
+        contracts=contractsSparse,
+        ).items():
+
+    step("Generating {}...", metric)
+
+    filename = '{metric}{frm}{to}.tsv'.format(
+        metric = metric,
+        frm = '-'+str(fromdate) if fromdate else '',
+        to  = '-'+str(todate) if todate else '',
+    )
+    result = generator(dates)
+    with io.open(filename,'w') as f:
+        f.write(utf8(result))
+    
+
+
+
+
