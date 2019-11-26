877ca41d09233edffd2e8b32febc2a066a3b343b
Mar Jene <mar.jene@somenergia.coop>
2019-11-26 12:53:29 +0100

    delete if from __call__

--- som_opendata/mapscale.py
+++ som_opendata/mapscale.py
@@ -5,7 +5,4 @@ class Scale(object):
         self.high = higher
 
     def __call__(self, val):
-        if val == self.low:
-            return 0
-        else:
-            return (val - self.low) / (self.high - self.low)
+        return (val - self.low) / (self.high - self.low)
