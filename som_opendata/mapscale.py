8ee1566ca914c5d6c8d9993127f3cd9298212b62
Mar Jene <mar.jene@somenergia.coop>
2019-11-26 12:52:37 +0100

    som_opendata.mapscale_test.Scale_test:test_scale_middle

--- som_opendata/mapscale.py
+++ som_opendata/mapscale.py
@@ -1,11 +1,11 @@
 class Scale(object):
 
-    def __init__(self, lower=0, higher=1000):
+    def __init__(self, lower=0, higher=100):
         self.low = lower
         self.high = higher
 
-    def __call__(self, point):
-        if point == self.low:
+    def __call__(self, val):
+        if val == self.low:
             return 0
         else:
-            return 1
+            return (val - self.low) / (self.high - self.low)
--- som_opendata/mapscale_test.py
+++ som_opendata/mapscale_test.py
@@ -4,10 +4,12 @@ class Scale_test(unittest.TestCase):
 
     def test_scale_min(self):
         scale = Scale(0, 1000)
-        point = scale(0)
-        self.assertEqual(point, 0)
+        self.assertEqual(scale(0), 0)
 
     def test_scale_max(self):
         scale = Scale(0, 1000)
-        point = scale(1000)
-        self.assertEqual(point, 1)
+        self.assertEqual(scale(1000), 1)
+
+    def test_scale_middle(self):
+        scale = Scale(0,1000)
+        self.assertEqual(scale(500), 0.5)
