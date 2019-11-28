ec0bfe504c7e2b7fe67adb0967cc385d9739fccc
Mar Jene <mar.jene@somenergia.coop>
2019-11-28 12:29:49 +0100

    adding rest of the word key

--- som_opendata/map.py
+++ som_opendata/map.py
@@ -12,9 +12,9 @@ def dataToTemplateDict(titol, subtitol, data):
             month = date.month,
         )
 
-    scale = LinearScale(higher=data["values"][0])
+    scale = LinearScale(higher=data.countries.ES["values"][0])
     color = Gradient('#e0ecbb','#384413')
-    totalValue = data["values"][0]
+ #   totalValue = data.ES["values"][0]
     for code, ccaa in data.countries.ES.ccaas.items():
         value = ccaa["values"][0]
         result.update({
@@ -22,4 +22,14 @@ def dataToTemplateDict(titol, subtitol, data):
             'percent_' + code: '{:.1f}%'.format(scale(value)*100).replace('.',','),
             'color_' + code: color(scale(value)),
             })
+    result.update({'number_00': data["values"][0]-data.countries.ES["values"][0]})
     return result
+
+
+def renderMap(data, template):
+    with open(template,'r') as svgTemplateFile:
+        svgTemplate = svgTemplateFile.read()
+
+    svgContent = svgTemplate.format(**data)
+
+    return svgContent
--- som_opendata/map_test.py
+++ som_opendata/map_test.py
@@ -1,9 +1,102 @@
+import b2btest
 import unittest
 from yamlns.dateutils import Date
 from yamlns import namespace as ns
-from .map import dataToTemplateDict
+from .map import dataToTemplateDict, renderMap
+
+fullData = ns.loads("""\
+    dates:
+    - 2013-01-01
+    values:
+    - 3208
+    countries:
+      ES:
+        name: España
+        values:
+        - 3208
+        ccaas:
+          '01':
+            name: Andalucia
+            values:
+            - 48
+          '02':
+            name: Aragón
+            values:
+            - 124
+          '03':
+            name: Asturias, Principado de
+            values:
+            - 13
+          '04':
+            name: Baleares, Islas
+            values:
+            - 235
+          '05':
+            name: Canarias
+            values:
+            - 0
+          '06':
+            name: Cantabria
+            values:
+            - 12
+          08:
+            name: Castilla - La Mancha
+            values:
+            - 28
+          '07':
+            name: Castilla y León
+            values:
+            - 24
+          09:
+            name: Cataluña
+            values:
+            - 2054
+          '10':
+            name: Comunidad Valenciana
+            values:
+            - 224
+          '11':
+            name: Extremadura
+            values:
+            - 14
+          '12':
+            name: Galicia
+            values:
+            - 24
+          '13':
+            name: Madrid, Comunidad de
+            values:
+            - 145
+          '14':
+            name: Murcia, Región de
+            values:
+            - 11
+          '15':
+            name: Navarra, Comunidad Foral de
+            values:
+            - 151
+          '16':
+            name: País Vasco
+            values:
+            - 53
+          '17':
+            name: Rioja, La
+            values:
+            - 37
+          '18':
+            name: Ceuta
+            values:
+            - 5
+          '19':
+            name: Melilla
+            values:
+            - 5
+""")
 
 class Map_Test(unittest.TestCase):
+
+    def setUp(self):
+        self.b2bdatapath = 'b2bdataMap'
 
     from somutils.testutils import assertNsEqual
 
@@ -24,6 +117,7 @@ class Map_Test(unittest.TestCase):
             subtitol: un subtítol
             year: 2019
             month: 1
+            number_00: 0
         """)
 
     def test_dataToTemplateDict_singleRegion(self):
@@ -46,6 +140,7 @@ class Map_Test(unittest.TestCase):
             subtitol: un subtítol
             year: 2019
             month: 1
+            number_00: 0
             number_01: 123
             percent_01: 100,0%
             color_01: '#384413'
@@ -58,7 +153,7 @@ class Map_Test(unittest.TestCase):
             countries:
               ES:
                 name: España
-                values: [123]
+                values: [143]
                 ccaas:
                   '01':
                     name: Andalucía
@@ -74,6 +169,7 @@ class Map_Test(unittest.TestCase):
             subtitol: un subtítol
             year: 2019
             month: 1
+            number_00: 0
             number_01: 123
             percent_01: 86,0%
             color_01: '#56691d'
@@ -81,3 +177,33 @@ class Map_Test(unittest.TestCase):
             percent_09: 14,0%
             color_09: '#cfe296'
         """)
+
+    def test_dataToTemplateDict_restWorld(self):
+        data = ns.loads("""\
+            dates: [2019-01-01]
+            values: [126]
+            countries:
+              ES:
+                name: España
+                values: [123]
+                ccaas:
+                  '01':
+                    name: Andalucía
+                    values: [123]
+            """)
+        result = dataToTemplateDict(titol="un títol", subtitol="un subtítol", data=data)
+
+        self.assertNsEqual(result, """\
+            titol: un títol
+            subtitol: un subtítol
+            year: 2019
+            month: 1
+            number_00: 3
+            number_01: 123
+            percent_01: 100,0%
+            color_01: '#384413'
+        """)
+
+    def _test_renderMap_singleRegion(self):
+        result = dataToTemplateDict(titol="un títol", subtitol="un subtítol", data=fullData)
+        self.assertB2BEqual(renderMap(data=result, template='MapaSocios-template.svg'))
