21435014c5b3b3cdea0788a0d81144ff664351a6
Mar Jene <mar.jene@somenergia.coop>
2019-12-03 12:33:53 +0100

    separated Log and Linear Scale tests
    Signed-off-by: Mar Jene <mar.jene@somenergia.coop>

--- som_opendata/scale_test.py
+++ som_opendata/scale_test.py
@@ -2,7 +2,8 @@ import unittest
 from .scale import LinearScale, LogScale
 from somutils import testutils
 
-class Scale_test(unittest.TestCase):
+
+class LinearScale_test(unittest.TestCase):
 
     def test_LinearScale_min(self):
         scale = LinearScale(0, 1000)
@@ -27,26 +28,6 @@ class Scale_test(unittest.TestCase):
     def test_LinearScale_minEqualMax(self):
         scale = LinearScale(100, 100)
         self.assertEqual(scale(100), 1)
-
-    def test_logScale_min(self):
-        scale = LogScale(lower=100, higher=10000)
-        self.assertEqual(scale(100), 0)
-
-    def test_logScale_max(self):
-        scale = LogScale(lower=100, higher=10000)
-        self.assertEqual(scale(10000), 1)
-
-    def test_logScale_middle(self):
-        scale = LogScale(lower=100, higher=10000)
-        self.assertEqual(scale(1000), 0.5)
-
-    def test_logScale_outRangeLow(self):
-        scale = LogScale(lower=100, higher=10000)
-        self.assertEqual(scale(-1), 0)
-
-    def test_logScale_outRangeHigh(self):
-        scale = LogScale(lower=100, higher=10000)
-        self.assertEqual(scale(100000), 1.5)
 
     def test_inverseLin_min(self):
         scale = LinearScale()
@@ -82,6 +63,35 @@ class Scale_test(unittest.TestCase):
         self.assertEqual(scale(scale.inverse(-0.25)), -0.25)
         self.assertEqual(scale(scale.inverse(1.25)), 1.25)
 
+    def test_LinearScale_minGreaterMax(self):
+        with self.assertRaises(ValueError) as context:
+            LinearScale(lower=1000, higher=10)
+        self.assertEqual(
+            "Lower value is greater than higher value",
+            str(context.exception)
+        )
+
+class LogScale_test(unittest.TestCase):
+
+    def test_logScale_min(self):
+        scale = LogScale(lower=100, higher=10000)
+        self.assertEqual(scale(100), 0)
+
+    def test_logScale_max(self):
+        scale = LogScale(lower=100, higher=10000)
+        self.assertEqual(scale(10000), 1)
+
+    def test_logScale_middle(self):
+        scale = LogScale(lower=100, higher=10000)
+        self.assertEqual(scale(1000), 0.5)
+
+    def test_logScale_outRangeLow(self):
+        scale = LogScale(lower=100, higher=10000)
+        self.assertEqual(scale(-1), 0)
+
+    def test_logScale_outRangeHigh(self):
+        scale = LogScale(lower=100, higher=10000)
+        self.assertEqual(scale(100000), 1.5)
     def test_inverseLog_min(self):
         scale = LogScale(higher=1000)
         self.assertEqual(scale(scale.inverse(0)), 0)
@@ -125,13 +135,5 @@ class Scale_test(unittest.TestCase):
             LogScale(lower=0, higher=10)
         self.assertEqual(
             "Log not defined for values <= 0",
-            str(context.exception)
-        )
-
-    def test_LinearScale_minGreaterMax(self):
-        with self.assertRaises(ValueError) as context:
-            LinearScale(lower=1000, higher=10)
-        self.assertEqual(
-            "Lower value is greater than higher value",
             str(context.exception)
         )
