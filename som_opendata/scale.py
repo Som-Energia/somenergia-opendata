e907af44653c7ee0b4b191db5cc3f501511eca04
Mar Jene <mar.jene@somenergia.coop>
2019-12-04 12:05:51 +0100

    early returns

--- som_opendata/scale.py
+++ som_opendata/scale.py
@@ -11,8 +11,7 @@ class LinearScale(object):
     def __call__(self, val):
         if self.low == self.high:
             return 1
-        else:
-            return (val - self.low) / (self.high - self.low)
+        return (val - self.low) / (self.high - self.low)
 
     def inverse(self, val):
         return self.low + val * (self.high - self.low)
@@ -23,7 +22,7 @@ class LogScale(object):
     def __init__(self, higher, lower=1):
         if higher < lower:
             raise ValueError("Lower value is greater than higher value")
-        elif lower <= 0:
+        if lower <= 0:
             raise ValueError("Log not defined for values <= 0")
         self.low = lower
         self.high = higher
@@ -31,8 +30,8 @@ class LogScale(object):
     def __call__(self, val):
         if val <= 0:
             return 0
-        else:
-            return log10(val / self.low) / log10(self.high / self.low)
+
+        return log10(val / self.low) / log10(self.high / self.low)
 
     def inverse(self, val):
         return self.low * (self.high /self.low) ** val
