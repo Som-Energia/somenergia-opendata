941cd1219a744d7e3241c14b80719c85afb414fd
Mar Jene <mar.jene@somenergia.coop>
2019-12-18 10:39:54 +0100

    method iterateLevel with generator to iterate a geolevel

--- som_opendata/map.py
+++ som_opendata/map.py
@@ -36,6 +36,26 @@ def maxValue(data, geolevel, frame):
 
     return processLevelMax(data.countries.ES, 0, 0, frame)
 
+
+def iterateLevel(data, geolevel):
+    geolevels = [
+        ('ccaa', 'ccaas'),
+        ('state', 'states'),
+        ('city', 'cities'),
+    ]
+
+    def processLevel(parentRegion, level):
+        singular, plural = geolevels[level]
+        for code, region in parentRegion[plural].items():
+            if singular != geolevel:
+                yield from processLevel(region, level + 1)
+                continue
+            yield code, region
+
+    yield from processLevel(data.countries.ES, 0)
+
+
+
 def dataToTemplateDict(data, colors, title, subtitle, colorScale='Log', locations=[], geolevel='ccaa', maxVal=None, frame=0):
     date = data.dates[frame]
     result = ns(
@@ -66,16 +86,8 @@ def dataToTemplateDict(data, colors, title, subtitle, colorScale='Log', location
                 'color_' + code: colors(scale(value)),
                 })
 
-    def processLevel(parentRegion, level):
-        singular, plural = geolevels[level]
-        for code, region in parentRegion[plural].items():
-            if singular != geolevel:
-                processLevel(region, level+1)
-                continue
-            value = region["values"][frame]
-            updateDict(code, value)
-
-    processLevel(data.countries.ES, 0)
+    for code, region in iterateLevel(data, geolevel):
+        updateDict(code, region["values"][frame])
 
     restWorld = data["values"][frame] - data.countries.ES["values"][frame]
     updateDict('00',restWorld)
