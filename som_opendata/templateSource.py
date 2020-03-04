8c8d34821497088b2b35cc395f38223e35515374
Mar Jene <mar.jene@somenergia.coop>
2020-03-04 10:28:58 +0100

    refactor: template files already translated and with style

--- som_opendata/templateSource.py
+++ som_opendata/templateSource.py
@@ -1,30 +1,18 @@
 from yamlns import namespace as ns
 from future.utils import iteritems
+from collections import defaultdict
 
 class TemplateSource(object):
 
-    def __init__(self, templates, styles, translations, legend):
+    def __init__(self, templates, legend):
         self.templates = templates
-        self.styles = styles
-        self.translations = translations
         self.legend = legend
-
-    def getRawTemplate(self, geolevel):
-        return self.templates[geolevel]
-
-    def getStyle(self, geolevel):
-        return self.styles.get(geolevel, '')
 
     def getLegend(self):
         return self.legend
 
     def getTemplate(self, geolevel, lang='en'):
-        #TODO: change to format_map() with a default that overrides __missing__ 
-        result = self.templates[geolevel]
-        for en_name, new_name in iteritems(self.translations[lang]):
-            result = result.replace('{'+en_name+'}', new_name, 1)
-        result = result.replace('{style}', self.styles.get(geolevel,''))
-        return result
+        return self.templates[geolevel][lang]
 
 
 
@@ -36,18 +24,13 @@ from pathlib2 import Path
 def loadMapData(relativePath='../maps'):
     myPath = os.path.abspath(os.path.dirname(__file__))
     dataPath = os.path.join(myPath, relativePath)
-    fileNames = ['template', 'style']
-    mapData = ns()
-    for name in fileNames:
-        mapData[name] = ns()
-        for datafile in glob.glob(os.path.join(dataPath, name+'_*.svg')):
-            geolevel = os.path.basename(datafile).replace('.svg', '').replace(name+'_', '')
-            mapData[name][geolevel] = Path(datafile).read_text(encoding='utf8')
-
-    translations = ns()
-    for datafile in glob.glob(os.path.join(dataPath, 'translations/*')):
-        lang = os.path.basename(datafile)
-        translations[lang] = ns.loads(Path(datafile).read_text(encoding='utf8'))
+    templates = ns()
+    for datafile in glob.glob(os.path.join(dataPath, 'mapTemplates/*')):
+        geolevel, lang = os.path.basename(datafile).split('_')
+        if not templates.get(geolevel):
+            templates[geolevel]= ns()
+        templates[geolevel][lang] = Path(datafile).read_text(encoding='utf8')
 
     legend = Path(dataPath + '/legend.svg').read_text(encoding='utf8')
-    return TemplateSource(templates=mapData.template, styles=mapData.style, translations=translations, legend=legend)
+
+    return TemplateSource(templates=templates, legend=legend)
--- som_opendata/templateSource_test.py
+++ som_opendata/templateSource_test.py
@@ -26,27 +26,12 @@ data = loadMapData()
 class TemplateSource_Test(unittest.TestCase):
     maxDiff = None
 
-    def test_getRawTemplate_geolevel(self):
-        self.maxDiff = None
-        self.assertNotEqual(len(data.getRawTemplate('ccaa')),0)
-        self.assertNotEqual(len(data.getRawTemplate('state')),0)
-        self.assertMultiLineEqual(data.getRawTemplate('dummy'), dummyTemplate)
-
-    def test_getStyle_geolevel(self):
-        self.assertNotEqual(len(data.getStyle('state')),0)
-        self.assertEqual(len(data.getStyle('ccaa')),0)
-
-    def test_loadMapData_translations(self):
-        translations = data.translations['ca']
-        self.assertEqual(len(translations),68)
-        self.assertEqual(translations['Catalonia'], 'Catalunya')
-
     def test_getLegend(self):
         self.assertNotEqual(len(data.getLegend()), '')
 
-    def test_getTemplate_dummyCa(self):
+    def test_getTemplate_dummyEn(self):
         self.maxDiff = None
-        result = data.getTemplate('dummy', lang='ca')
+        result = data.getTemplate('dummy', lang='en')
         self.assertMultiLineEqual(result,
 u"""<svg xmlns="http://www.w3.org/2000/svg" width="480" version="1.1" height="300">
   <text y="40" x="170" style="text-anchor:middle">Title: {title}</text>
@@ -54,7 +39,7 @@ u"""<svg xmlns="http://www.w3.org/2000/svg" width="480" version="1.1" height="30
   <text y="80" x="170" style="text-anchor:middle">Year: {year}</text>
   <text y="100" x="170" style="text-anchor:middle">Month: {month}</text>
   <text y="110" x="50" style="text-anchor:middle">Andalusia</text>
-  <text y="110" x="320" style="text-anchor:middle">Catalunya</text>
+  <text y="110" x="320" style="text-anchor:middle">Catalonia</text>
   <circle cy="180" cx="100" r="60" fill="{color_01}"/>
   <text y="180" x="100" style="text-anchor:middle">{number_01}</text>
   <text y="200" x="100" style="text-anchor:middle">{percent_01}</text>
@@ -62,5 +47,4 @@ u"""<svg xmlns="http://www.w3.org/2000/svg" width="480" version="1.1" height="30
   <text y="180" x="240" style="text-anchor:middle">{number_09}</text>
   <text y="200" x="240" style="text-anchor:middle">{percent_09}</text>
   <text y="280" x="260" style="text-anchor:middle">{legend}</text>
-  
 </svg>""")
