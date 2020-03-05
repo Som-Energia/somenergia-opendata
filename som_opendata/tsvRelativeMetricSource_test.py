e2be6379325ccb1f774dfe97f5ca86ddf10801b1
Mar Jene <mar.jene@somenergia.coop>
2020-03-05 12:35:39 +0100

    using tsv variables for tests

--- som_opendata/tsvRelativeMetricSource_test.py
+++ som_opendata/tsvRelativeMetricSource_test.py
@@ -1,27 +1,28 @@
 import b2btest
 import unittest
 from yamlns import namespace as ns
+from .distribution import parse_tsv, tuples2objects
 from .tsvRelativeMetricSource import (
         TsvRelativeMetricSource,
         loadTsvRelativeMetric,
         getFieldBy,
     )
 
+headers = u"code\tname\tvalue"
+catalunya = u"09\tCatalunya\t7416237"
+andalusia = u"01\tAndalucia\t8388875"
+
 
 class TsvRelativeMetricSource_Test(unittest.TestCase):
 
     def test_getFieldBy_nameByCode(self):
-        data= [
-                {'code':'01', 'name': 'Andalucia', 'value': '50000'},
-                {'code':'09', 'name': 'Catalunya', 'value': '70000'},
-            ]
+        rawData = '\n'.join([headers,catalunya,andalusia])
+        data= tuples2objects(parse_tsv(rawData))
         result = getFieldBy(data=data, field='name', by='code')
         self.assertEqual(result, {'01': 'Andalucia', '09': 'Catalunya'})
 
     def test_getFieldBy_valueByCode(self):
-        data= [
-                {'code':'01', 'name': 'Andalucia', 'value': '50000'},
-                {'code':'09', 'name': 'Catalunya', 'value': '70000'},
-            ]
+        rawData = '\n'.join([headers,catalunya,andalusia])
+        data= tuples2objects(parse_tsv(rawData))
         result = getFieldBy(data=data, field='value', by='code')
-        self.assertEqual(result, {'01': '50000', '09': '70000'})
+        self.assertEqual(result, {'01': '8388875', '09': '7416237'})
