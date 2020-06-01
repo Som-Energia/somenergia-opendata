73c10d88402cc5d50e7de03cb2f13ab34b137084
Mar Jene <mar.jene@somenergia.coop>
2020-06-01 11:58:07 +0200

    Fix documentation

--- doc/api_project.js
+++ doc/api_project.js
@@ -9,8 +9,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-03-16T09:19:46.548Z",
+    "time": "2020-06-01T09:57:11.681Z",
     "url": "http://apidocjs.com",
-    "version": "0.20.0"
+    "version": "0.23.0"
   }
 });
