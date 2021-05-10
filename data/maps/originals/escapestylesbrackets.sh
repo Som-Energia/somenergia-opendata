c25eea795af314d30671af160a0eac7e3a7dcaed
David García Garzón <david.garcia@somenergia.coop>
2021-05-10 22:00:04 +0200

    escapestylesbrackets: formatted sed commands

--- data/maps/originals/escapestylesbrackets.sh
+++ data/maps/originals/escapestylesbrackets.sh
@@ -2,4 +2,10 @@
 # This script escapes all curly brackets between <style></style> tags
 # in order to be safe for python format.
 
-sed '/<style/,/<\/style/ s/{/{{/g; /<style/,/<\/style/ s/}/}}/g; /<style/,/<\/style/ s/{\({[a-zA-Z0-9_][a-zA-Z0-9_]*}\)}/\1/g;' "$@"
+sed '
+/<style/,/<\/style/ s/{/{{/g;
+/<style/,/<\/style/ s/}/}}/g;
+/<style/,/<\/style/ s/{\({[a-zA-Z0-9_][a-zA-Z0-9_]*}\)}/\1/g;
+' "$@"
+
+
