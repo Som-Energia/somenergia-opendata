4a4938792a6e2d9bfb276111ffc861192f6175d7
David García Garzón <david.garcia@somenergia.coop>
2021-07-05 10:56:29 +0200

    plantpower.sql: peak and nominal powers

--- som_opendata/queries/plantpower.sql
+++ som_opendata/queries/plantpower.sql
@@ -22,22 +22,23 @@ FROM (
 		nominal_power_kw as value
 	from (
 	values
-		('25120','2012-01-01'::date,'Lleida','388828084', 140),
-		('17148','2012-01-01'::date,'Riudarenes_SM','501215455', 20),
-		('17148','2012-01-01'::date,'Riudarenes_BR','501215456', 20),
-		('17148','2012-01-01'::date,'Riudarenes_ZE','501215457', 18),
-		('25228','2013-01-01'::date,'Torrefarrera','28302095', 90),
-		('08112','2013-01-01'::date,'Manlleu_Piscina','28301116', 90),
-		('08112','2013-01-01'::date,'Manlleu_Pavello','28301134', 100),
-		('46193','2013-01-01'::date,'Picanya','70312', 290),
-		('25230','2013-01-01'::date,'Torregrossa','28301107', 500),
-		('47114','2015-01-01'::date,'Valteina','80429506', 1000),
-		('41006','2016-01-01'::date,'Alcolea','501600324', 2160),
-		('41055','2018-01-01'::date,'Matallana','502016144', 2000),
-		('05074','2019-01-01'::date,'Fontivsolar','501815908', 990),
-		('41055','2019-01-01'::date,'Florida','44711885', 1500),
-		('04090','2020-01-01'::date,'Tahal','88300909', 841)
-	) as plant(city_code, first_date, plant_name, meter_name, nominal_power_kw)
+		('25120','2012-01-01'::date,'Lleida',         '388828084',   99,  104),
+		('17148','2012-01-01'::date,'Riudarenes_SM',  '501215455',   20,   22),
+		('17148','2012-01-01'::date,'Riudarenes_BR',  '501215456',   20,   22),
+		('17148','2012-01-01'::date,'Riudarenes_ZE',  '501215457',   18,   19),
+		('08112','2013-01-01'::date,'Manlleu_Piscina',' 28301116',   90,   95),
+		('08112','2013-01-01'::date,'Manlleu_Pavello', '28301134',  100,  109),
+		('25228','2013-03-01'::date,'Torrefarrera',    '28302095',   90,   98),
+		('46193','2013-03-01'::date,'Picanya',            '70312',  290,  335),
+		('25230','2013-09-01'::date,'Torregrossa',     '28301107',  500,  500),
+		('47114','2015-08-01'::date,'Valteina',        '80429506', 1000, 1000),
+		('41006','2016-05-01'::date,'Alcolea',        '501600324', 1890, 2160),
+		('41055','2018-09-01'::date,'Matallana',      '502016144', 2000, 2376),
+		('05074','2019-01-01'::date,'Fontivsolar',    '501815908',  800,  990),
+		('41055','2019-10-01'::date,'Florida',         '44711885', 1400, 1658),
+		('04090','2020-10-01'::date,'Tahal',           '88300909',  720,  841),
+		('18152',        null::date,'Llanillos',             null, 3200, 3800)
+	) as plant(city_code, first_date, plant_name, meter_name, nominal_power_kw, peak_power_kw)
 	left join giscedata_registrador as registry
 	on registry.name = plant.meter_name
 	left join res_municipi as city
