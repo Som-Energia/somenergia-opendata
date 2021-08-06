4f5954d48ba5de46e72f720e2cd5237cb31fd832
David García Garzón <david.garcia@somenergia.coop>
2021-08-06 18:57:15 +0200

    contracts query in the new system

---
+++ som_opendata/queries/contracts.sql
@@ -0,0 +1,11 @@
+--- All contracts
+SELECT
+	polissa.id as id,
+	polissa.data_alta as first_date,
+	polissa.data_baixa as last_date,
+	cups.id_municipi as city_id,
+	NULL::integer as country_id,
+	TRUE
+FROM giscedata_polissa AS polissa
+INNER JOIN giscedata_cups_ps AS cups
+	ON polissa.cups = cups.id
