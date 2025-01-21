a73ba9cd73986e5020398de5c444610766fe295e
pol <pol.monso@somenergia.coop>
2025-01-21 12:59:09 +0100

    new: adapt to new contract with dades jardiner for plants

--- som_opendata/queries/plantproduction.sql
+++ som_opendata/queries/plantproduction.sql
@@ -1,25 +1,27 @@
 SELECT
-	city.countrycode AS codi_pais,
-	city.country AS pais,
-	city.regioncode AS codi_ccaa,
-	city.region AS comunitat_autonoma,
-	city.provincecode AS codi_provincia,
-	city.province AS provincia,
-	city.inecode AS codi_ine,
-	city.name AS municipi,
+	codi_pais,
+	pais,
+	codi_ccaa,
+	comunitat_autonoma,
+	codi_provincia,
+	provincia,
+	codi_ine,
+	municipi,
 	{} -- here go the count columns
 FROM (
 	SELECT
-		plant.municipality as city_id,
-		energy.export_energy_wh/1000 as value,
-		energy.time as time
-	FROM prod.view_plant_export_energy_monthly AS energy
-	LEFT JOIN plant
-	ON energy.plant_id = plant.id
-	WHERE plant.description != 'SomRenovables'
+		codi_pais,
+		pais,
+		codi_ccaa,
+		comunitat_autonoma,
+		codi_provincia,
+		provincia,
+		codi_ine,
+		municipi,
+		energia_exportada_kwh as value,
+		month as time
+	FROM dbt_prod.dm_plant_production_monthly__opendata
 ) AS item
-LEFT JOIN municipality AS city
-ON item.city_id = city.id
 GROUP BY
 	codi_pais,
 	codi_ccaa,
